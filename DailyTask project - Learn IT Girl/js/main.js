af0403ed553620420011fd60faf700615ad72e7d
Agata Wisniewska <ag.e.wisniewska@gmail.com>
2016-04-04 21:31:52 +0200

    adding documentation to code

--- DailyTask project - Learn IT Girl/js/main.js
+++ DailyTask project - Learn IT Girl/js/main.js
@@ -13,6 +13,8 @@ $('#third').click(function (){
 		$('#thirdul').toggle();
 });
 
+
+
 $('.box').dblclick(function() {
 	$(this).next('.innerbox').toggle();
 	return false
@@ -74,7 +76,7 @@ $('#creator2').click(function() {
     $('#Modal').modal('hide');
 
 });
-// update task
+// update task - here I fill the update modal form with data from localStoarge
 $('body').on('click', 'div.edit', function() {
   $("#Modal").modal();
 
@@ -91,26 +93,16 @@ $('body').on('click', 'div.edit', function() {
     document.getElementById("datepicker4").value =  object.endDate;
     $("input[name=optradio2][value=" + object.priority + "]").prop('checked', true);
 
-
-
-
-
-
 });
 // function executes on trash click which removes data (div) from main view and localStorage
 $('body').on('click', 'div.trash', function() {
-
    $(this).parent().remove();
    var id = $(this).parent().attr('id');
-   console.log(id);
-//    if (localStorage(key) == id ) {
-    if(id in localStorage){
-       
+      if(id in localStorage){
         localStorage.removeItem(id);  
           }
-//    }
 });
-
+// Here I run functions on mouseover effect which help me to change task status and placed them in localStroage.
 $('body').on('mouseover', 'div#dn', function () {
    
    $(this).parent().parent().parent().siblings('h3').find('p').css("text-decoration", "line-through"); 
@@ -119,14 +111,9 @@ $('body').on('mouseover', 'div#dn', function () {
    var object = JSON.parse(value);
    object.taskstatus = "done";
    localStorage.setItem(id, JSON.stringify(object));   
-
-
-console.log(localStorage);
-
-
 });
+
 $('body').on('mouseover', 'div#do', function () {
-   
    $(this).parent().parent().parent().parent().append('<span class="label label-success">doing</span>');
     var id = $(this).parent().parent().parent().parent().attr('id');
    var value = localStorage[id];
@@ -134,42 +121,32 @@ $('body').on('mouseover', 'div#do', function () {
    object.taskstatus = "doing";
    localStorage.setItem(id, JSON.stringify(object));   
 
-
-console.log(localStorage);
-
-
 });
-
+// Here I can display all objects from localStorage which category is "work" and hide those which category is "home" The function is run on "work" button click.
 $('#work').click(function () {
   $('.zadania').empty();
     for(var i=0; i<localStorage.length; i++) {
          var key = localStorage.key(i);
          var value = localStorage[key];
-         
          var myobj = JSON.parse(value);
-         console.log(typeof(myobj));
          var category = myobj.category;
-         
             if (category === "Work") {
                  var div  = '<div class ="box"  id="' + key + '">'+ trash + edit + hamburgermenu +  '<h3>' + myobj.startDate + " "  + myobj.endDate + checkStatus(myobj.category, myobj.desc, myobj.taskstatus)  +  '</h3>' +'</div>';
                   $('.zadania').append(div)
             }
-
        }
 });
 
 
-
+// Here I can diplay all objects from localStorage which category is "home" and hide those which category is "work". The function is run on "home" button click
 $('#home').click(function () {
   $('.zadania').empty();
     for(var i=0; i<localStorage.length; i++) {
          var key = localStorage.key(i);
          var value = localStorage[key];
-         
          var myobj = JSON.parse(value);
          console.log(typeof(myobj));
          var category = myobj.category;
-         
             if (category === "Home") {
                  var div  = '<div class ="box"  id="' + key + '">'+ trash + edit + hamburgermenu +  '<h3>' + myobj.startDate + " "  + myobj.endDate + checkStatus(myobj.category, myobj.desc, myobj.taskstatus)  +  '</h3>' +'</div>';
                   $('.zadania').append(div)
--- DailyTask project - Learn IT Girl/js/main2.js
+++ DailyTask project - Learn IT Girl/js/main2.js
@@ -1,116 +1,92 @@
-
+// Here I have a function to create a single task
 
 function createTask() {
-
-
-
+// Firstly I read all data from modal form available on button "create task" click
 	var status = document.getElementById("taskstatus").value;
-
 	var selected = $( "#selectpicker option:selected" ).text();
-
-	var description = document.getElementById('taskdesc').value;
-		
-	var startDate = document.getElementById("datepicker").value;
-		
-	var endDate = document.getElementById("datepicker2").value;
-		
+	var description = document.getElementById('taskdesc').value;	
+	var startDate = document.getElementById("datepicker").value;	
+	var endDate = document.getElementById("datepicker2").value;	
 	var optradio = document.getElementsByName('optradio');
 	var optradio;
-		for(var i = 0; i < optradio.length; i++){
-	    	if(optradio[i].checked){
-	        	optradio = optradio[i].value;
-	        }
-	    }
-
-
-
+	for(var i = 0; i < optradio.length; i++){
+		if(optradio[i].checked){
+			optradio = optradio[i].value;
+		}
+	}
+	// then I create and object TASK
 	var task = {}
-		task.id = "id" + Math.random().toString(16).slice(2)
-
+	// next I create my object key. It is div ID  which is randomously generated
+	task.id = "id" + Math.random().toString(16).slice(2)
+// here I add value to object task
 	task.taskstatus = status;
 	task.category = selected;
 	task.desc = description;
 	task.startDate = startDate;
 	task.endDate = endDate;
 	task.priority = optradio;
+
+	if(typeof(Storage)!=="undefined")
+		{
+		// here I create an object in localStorage. Because of the fact that localStorage content are only strings I need to change my object into string using JSON
+		localStorage.setItem(task.id,JSON.stringify(task));
 	
-
-	console.log(task);
-
-		if(typeof(Storage)!=="undefined")
-  				{
-			localStorage.setItem(task.id,JSON.stringify(task));
-
-				task = localStorage.getItem(task.id); 
-				task = JSON.parse(task);
-			
-				
-
- 			 }
-			else
-				  {
-				 alert("oh no...")
-				  }
-	
-	
-	for (var i = 0; i < localStorage.length; i++){
-
-		var div	=	'<div class	="box"	id="' + task.id + '">'+ trash + edit + hamburgermenu +	'<h3>' 	+ task.desc + " "
-		+ task.startDate + " " +  task.endDate +  " " + optradio + " " + selected + '</h3>' + '</div>';
-		
 		}
-
-$(".zadania").empty()
+	else
+	{
+		alert("oh no...")
+	}
 	
+	//  Here I clear everything which is available in div.zadania to avoid addind duplicate content
+	$(".zadania").empty()
+	//  Here I call a function to print all of my objects available in localStorage on page
 	printKeys();
-
-
-	
+	//  Here I clear from from all data inserted
 	clearForm();
 
 
- 
+
 
 }
 
 var trash = '<div class="trash"> \
-		<span class="glyphicon glyphicon-trash" aria-hidden="true"> \
-		</span>\
-	</div>'
+<span class="glyphicon glyphicon-trash" aria-hidden="true"> \
+</span>\
+</div>'
 
 var edit = '<div class="edit"> \
-		<span class="glyphicon glyphicon-edit" aria-hidden="true">\
-		 </span>\
-	</div>'
+<span class="glyphicon glyphicon-edit" aria-hidden="true">\
+</span>\
+</div>'
 
 
-												       
+
 var hamburgermenu= '<div class="dropdown hamburger"> \
-		<span class="glyphicon glyphicon-star" aria-hidden="true"> \
-			<div class="dropdown-content">\
-				<div id="td">todo</div>\
-				<div id="do">doing</div>\
-				<div id="dn">done</div> \
-			</div>\
-		</span>\
-	</div>'
+<span class="glyphicon glyphicon-star" aria-hidden="true"> \
+<div class="dropdown-content">\
+<div id="td">todo</div>\
+<div id="do">doing</div>\
+<div id="dn">done</div> \
+</div>\
+</span>\
+</div>'
 
 
 function printKeys() {
 	for(var i=0; i<localStorage.length; i++) {
-	    var key = localStorage.key(i);
-	    var value = localStorage[key];
-	    console.log(typeof(value));
-	    
-	    console.log(key + " => " + value)
+		var key = localStorage.key(i);
+		var value = localStorage[key];
+		console.log(typeof(value));
 
-	   	var myobj = JSON.parse(localStorage[key]);
-	    console.log(typeof(myobj));
-	 
+		console.log(key + " => " + value)
+
+		var myobj = JSON.parse(localStorage[key]);
+		console.log(typeof(myobj));
+
 	    // var div	=	'<div class	="box"	id="' + key + '">'+ trash + edit + hamburgermenu +	'<h3>' + myobj.startDate + " "  + myobj.endDate + checkStatus(myobj.category, myobj.desc, myobj.taskstatus)  +  '</h3>' +'</div>';
-	   	var div = '<div class	="box"	id="' + key + '">' + trash + edit + hamburgermenu + '<h3>' + myobj.startDate + " "  + myobj.endDate + checkStatus(myobj.category, myobj.desc, myobj.taskstatus)+'</h3>' +'</div>'
+	    var div = '<div class	="box"	id="' + key + '">' + trash + edit + hamburgermenu + '<h3>' + myobj.startDate + " "  + myobj.endDate + checkStatus(myobj.category, myobj.desc, myobj.taskstatus)+'</h3>' +'</div>'
 
-	   	$('.zadania').append(div);
+	    $('.zadania').append(div);
 	}
 
 }
@@ -148,19 +124,19 @@ function updateTask() {
 	// selected = checkValue();
 
 	var description = document.getElementById('taskdescription').value;
-		
+
 	var startDate = document.getElementById("datepicker3").value;
-		
+
 	var endDate = document.getElementById("datepicker4").value;
-		
+
 	var optradio = document.getElementsByName('optradio2');
 	
 	var optradio;
-		for(var i = 0; i < optradio.length; i++){
-	    	if(optradio[i].checked){
-	        	optradio = optradio[i].value;
-	        }
-	    }
+	for(var i = 0; i < optradio.length; i++){
+		if(optradio[i].checked){
+			optradio = optradio[i].value;
+		}
+	}
 
 	var task = {}
 
@@ -177,27 +153,27 @@ function updateTask() {
 
 
 	if(typeof(Storage)!=="undefined")
-  				{
+	{
 
-  			task_temp = localStorage.getItem(task.id); 
-			task_temp = JSON.parse(task_temp);
+		task_temp = localStorage.getItem(task.id); 
+		task_temp = JSON.parse(task_temp);
 
-			task.taskstatus = task_temp.taskstatus;		
-			localStorage.setItem(task.id, JSON.stringify(task));
-			
-				
+		task.taskstatus = task_temp.taskstatus;		
+		localStorage.setItem(task.id, JSON.stringify(task));
 
- 			 }
-			else
-				  {
-				 alert("oh no...")
-				  }
+
+
+	}
+	else
+	{
+		alert("oh no...")
+	}
 	
 
- 			$('.zadania').empty();
-			printKeys();
+	$('.zadania').empty();
+	printKeys();
 
-  
+
 
 }
 
