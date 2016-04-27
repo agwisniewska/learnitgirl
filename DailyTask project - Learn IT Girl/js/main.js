68c3f3fd36c2f965f8ad55cbc5a4fbe5955ba0d0
Agata Wisniewska <ag.e.wisniewska@gmail.com>
2016-04-27 23:08:07 +0200

    digital clock refactoring

--- DailyTask project - Learn IT Girl/js/main.js
+++ DailyTask project - Learn IT Girl/js/main.js
@@ -1,5 +1,40 @@
 $(document).ready(function() {
-	
+
+
+
+
+var monthNames = [ "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" ]; 
+var dayNames= ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
+
+// Create a newDate() object
+var newDate = new Date();
+// Extract the current date from Date object
+newDate.setDate(newDate.getDate());
+// Output the day, date, month and year   
+// $('#Date').html(dayNames[newDate.getDay()] + " " + newDate.getDate() + ' ' + monthNames[newDate.getMonth()] + ' ' + newDate.getFullYear());
+
+setInterval( function() {
+  // Create a newDate() object and extract the seconds of the current time on the visitor's
+  var seconds = new Date().getSeconds();
+  // Add a leading zero to seconds value
+  $("#sec").html(( seconds < 10 ? "0" : "" ) + seconds);
+  },1000);
+  
+setInterval( function() {
+  // Create a newDate() object and extract the minutes of the current time on the visitor's
+  var minutes = new Date().getMinutes();
+  // Add a leading zero to the minutes value
+  $("#min").html(( minutes < 10 ? "0" : "" ) + minutes);
+    },1000);
+  
+setInterval( function() {
+  // Create a newDate() object and extract the hours of the current time on the visitor's
+  var hours = new Date().getHours();
+  // Add a leading zero to the hours value
+  $("#hours").html(( hours < 10 ? "0" : "" ) + hours);
+    }, 1000); 
+
+
 $('#first').click(function(event)  {
 	event.stopPropagation();
   $('#firstul').toggle();
@@ -74,7 +109,7 @@ $('body').on('dblclick', '.box', function () {
          if (object.codetoopen != '') {
               var result = prompt ("Podaj właściwy kod, żeby zobaczyć zadanie");
               if (result == object.codetoopen) {
-                  $('box.id').child().show();
+                  alert("ok!")
               }
               else {
                 alert("Podałeś niepoprawny kod!")
@@ -284,7 +319,7 @@ $('#next').click(function () {
 
      });
 
-  $('#clock').timeTo();
+
 
 });
 
